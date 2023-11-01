6c7f6effef2afb7e7953e3213eef97108b275ef2
Zihadul Islam <zihadislam2429@gmail.com>
2023-11-01 22:35:31 +0600

    delete operation

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -10,10 +10,17 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const employee = await service.getAllEmployees(req.params.id);
+  const employee = await service.getEmployeeById(req.params.id);
   if (employee.length == 0)
     return res.status(404).json("no record with given id :" + req.params.id);
   else res.send(employee);
+});
+
+router.delete("/:id", async (req, res) => {
+  const affectedRows = await service.deleteEmployee(req.params.id);
+  if (affectedRows == 0)
+    res.status(404).json("no record with given id :" + req.params.id);
+  else res.send("delected successfully");
 });
 
 module.exports = router;
--- services/employee.service.js
+++ services/employee.service.js
@@ -1,15 +1,17 @@
 const db = require("../db");
 
 module.exports.getAllEmployees = async () => {
-  const [records] = await db
-    .query("SELECT * FROM employees")
-    .catch((err) => console.log(err));
+  const [records] = await db.query("SELECT * FROM employees");
   return records;
 };
 
-module.exports.getAllEmployees = async (id) => {
-  const [record] = await db
-    .query("SELECT * FROM employees WHERE id = ?", [id])
-    .catch((err) => console.log(err));
+module.exports.getEmployeeById = async (id) => {
+  const [record] = await db.query("SELECT * FROM employees WHERE id = ?", [id]);
+
   return record;
+};
+
+module.exports.deleteEmployee = async (id) => {
+  const [record] = await db.query("DELETE FROM employees WHERE id = ?", [id]);
+  return record.affectedRows;
 };
