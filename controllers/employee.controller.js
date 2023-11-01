9965634506732f0e458bd3fe43288a7f5248f2c2
Zihadul Islam <zihadislam2429@gmail.com>
2023-11-01 21:45:27 +0600

    retrieve data by id

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -9,4 +9,11 @@ router.get("/", async (req, res) => {
   res.send(rows);
 });
 
+router.get("/:id", async (req, res) => {
+  const employee = await service.getAllEmployees(req.params.id);
+  if (employee.length == 0)
+    return res.status(404).json("no record with given id :" + req.params.id);
+  else res.send(employee);
+});
+
 module.exports = router;
--- services/employee.service.js
+++ services/employee.service.js
@@ -1,8 +1,15 @@
 const db = require("../db");
 
 module.exports.getAllEmployees = async () => {
-  const [rows] = await db
+  const [records] = await db
     .query("SELECT * FROM employees")
     .catch((err) => console.log(err));
-  return rows;
+  return records;
+};
+
+module.exports.getAllEmployees = async (id) => {
+  const [record] = await db
+    .query("SELECT * FROM employees WHERE id = ?", [id])
+    .catch((err) => console.log(err));
+  return record;
 };
