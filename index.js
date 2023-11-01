ecdfde99d831d2636d253fb70713213e8d4e82eb
Zihadul Islam <zihadislam2429@gmail.com>
2023-11-01 21:35:33 +0600

    global error handler

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express"),
   app = express();
+require("express-async-errors");
 const database = require("./db"),
   employeeRoutes = require("./controllers/employee.controller");
 
 //middleware
 app.use("/api/employees", employeeRoutes);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send("Something went wrong!");
+});
 
 database
   .query("SELECT 1")
